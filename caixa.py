b603ce96c755f54d0f9f11d9c3b8fd3f6f253264
Maycon Henrique <mayconhenrique360@gmail.com>
2023-06-12 15:32:55 -0300

    atualizar banco

--- caixa.py
+++ caixa.py
@@ -1,34 +1,16 @@
 from conta import contas
 import pandas
-from PySimpleGUI import PySimpleGUI as sg
 
-<<<<<<< HEAD
-#layou
-sg.theme('Reddit')
-layout = [
-    [sg.Text('Conta:'), sg.Input(key='Conta')],
-    [sg.Button('Mostrar')]
-]
-=======
+print("**BEM VINDO AO BANCO HENRIQUE**")
+planilha = pandas.read_excel('contas.xlsx')
+usuario = int(input("Digite seu CPF:"))
+senha= int(input("Digite sua senha:"))
 
-conta = int(input())
-contas.extrato(conta)
+if contas.validar(usuario, senha) == True:
+     print("Logado com sucesso!")
+else: 
+     print("Conta não existe.")
 
 
 
 
-
->>>>>>> 469fbce9c4184a867cf7e839586f6e685ab14510
-
-#janela
-janela = sg.Window("Banquinho xereca", layout)
-
-#ler os eventos
-while True:
-    eventos, valores = janela.read()
-    if eventos == sg.WINDOW_CLOSED:
-        break
-    if eventos == 'Mostrar':
-        if valores['Conta'] == '0':
-            sg.Print(sg.Window("Saldo:{}".format(contas.extrato())))
-            
--- conta.py
+++ conta.py
@@ -7,29 +7,32 @@ class contas:
     def __init__(self, nome):
         self.nome = nome
 
-    def criarcontas():
+    def retornarcontas(usuario, senha):
         planilha_caminho = 'contas.xlsx'
         planilha = pandas.read_excel(planilha_caminho)
         lista = []
-        print("Criando contas...")
         for index, row in planilha.iterrows():
             nome = row["NOME"]
             agencia = row["AGENCIA"]
             numero_conta = row["CONTA"]
             saldo = row["SALDO"]
             limite = row["LIMITE"]
+            cpf = (row["CPF"])
+            senha = (row["SENHA"])
             lista.append(contas(nome))
             lista.append(contas(agencia))
             lista.append(contas(numero_conta))
             lista.append(contas(saldo))
-            print("Nome:{}\nAgencia:{}\nConta:{}\nSaldo:{}\n".format(row["NOME"], row["AGENCIA"], row["CONTA"], row["SALDO"]))
+            lista.append(contas(limite))
+            lista.append(contas(cpf))
+            lista.append(contas(senha))
+            print("Nome:{}\nAgencia:{}\nConta:{}\nSaldo:{}\nCPF:{}\n".format(row["NOME"], 
+            row["AGENCIA"], row["CONTA"], row["SALDO"], row["CPF"], row["SENHA"]))
 
     def extrato(conta):
         planilha = pandas.read_excel('contas.xlsx')
         planConta = planilha["SALDO"]
-        print(planConta[conta])
-        valor ={"SALDO": planConta}
-
+        print("Saldo da conta selecionada:R${}".format(planConta[conta]))
 
     def retornarcontas():
         planilha_caminho = 'contas.xlsx'
@@ -58,6 +61,32 @@ class contas:
         df = pandas.DataFrame(valor).round(1)
         df.to_excel('contas.xlsx', index = False)
         
+
+    def validar(usuario, senha):
+        planilha = pandas.read_excel('contas.xlsx')
+        planNome = planilha["NOME"]
+        planAg = planilha["AGENCIA"]
+        planConta = planilha["CONTA"]
+        planSaldo = planilha["SALDO"].astype("int64")
+        planLimite = planilha["LIMITE"]
+        planCpf = planilha["CPF"]
+        planSenha = planilha["SENHA"]
+        valor = {
+            "NOME":planNome,
+            "AGENCIA": planAg,
+            "CONTA": planConta,
+            "SALDO": planSaldo,
+            "LIMITE": planLimite,
+            "CPF": planCpf,
+            "SENHA": planSenha
+        }
+        df = pandas.DataFrame(valor).round(1)
+        df.to_excel('contas.xlsx', index = False)
+
+        if usuario in planCpf and senha in planSenha:
+            return True
+        else:
+            return False
 
         
     
